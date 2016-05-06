8a034827eaa38c25cea27dd817915714a8716568
JackMasterson <jackmasterson5@gmail.com>
2016-05-05 22:41:24 -0400

    firstRun
    has the view model sections as well as adding a lot of hard-coded html;
    seeing what works better

--- js/script.js
+++ js/script.js
@@ -2,51 +2,72 @@ var model = {
   sections: 
   [
     {
-      title: "Our Goal is Simple",
+      title: "Everybody's got the funny in them. We just bring it out.",
       subHead: [
         {
-          listHead: "Give away backpacks full of essentials to as many" +
-            " homeless people as we can.",
-          listItem: ["By partnering with St. John's Soup Kitchen in Newark, NJ," +
-        " we're collecting new and gently used backpacks, filling them" +
-        " with the materials we gather (listed below), and handing them out at " +
-        "mealtimes to those who need them."]
-        }
+          listHead: "Sign up for our improv, acting, and writing workshops " +
+            "and tap into your creative side.",
+          listItem: "All the world's a stage, and all the men and women" +
+            " merely players. - The Bard, William Shakespeare",
+          src: 'img/shakespeare.png',
+          id: 'note',
+          href: 'http://shorefunimprov.com/upcoming-events.html',
+          hrefTxt: 'Workshops'
+        },
       ],
       color: "#15c188"
     },
     {
-      title: "What We're Looking For",
+      title: "Who We Are",
       subHead: [
         {
-          listHead: "Toiletries", 
-          listItem: ["Razors", " Baby Wipes", " Shaving Cream", " Body Wash",
-            " Shampoo", " Toothbrush/toothpaste", " Floss", " Mouthwash",
-            " Deodorant"]
+          listHead: "Mr. Harvey", 
+          listItem: ["Over 22 Years Teaching Experience",                     
+                    " Manasquan High School Teacher of the Year",              
+                    " Who's Who Among American Teachers",                   
+                    " Teacher of AP Literature & Composition, Writing",       
+                    " Honors English, Screenwriting, & Acting", 
+                    " Frequent MHS Drama Club Asst. Director & Actor ",
+                    " MA Rutgers University",
+                    " UCLA School of Film, Theater & Television",
+                    " Philadelphia ComedySportz Team Founding Member"],
+          src: "img/harvey.jpg",
+          id: 'harvey',
+          href: 'http://www.manasquanschools.org/site/Default.aspx?PageID=691',
+          hrefTxt: 'School Page'
         },
         {
-          listHead: "Clothing", 
-          listItem: [" Heavy Scarves", " Dark T-Shirts", " Sweatpants",
-            " Socks", " Underwear"]
-        },
-        {
-          listHead: "Miscellaneous",
-          listItem: [" Backpacks", " Reusable Water Bottles", " Band-Aids",
-            " Sunscreen", " Non-perishables"]
+          listHead: "Mr. Mawn", 
+          listItem: ["Over 21 Years Teaching Experience",
+                    " Manasquan High School Teacher of the Year",
+                    " Who's Who Among American Teachers",
+                    " Teacher of AP Composition and Writing",   
+                    " Honors English, and Journalism",           
+                    " Advisor for MHS Newspaper The Blue & Gray",
+                    " Advisor for MHS Literary Magazine",
+                    " MA Teachers College, Columbia University"],
+          src: "img/mawn.jpg",
+          id: 'mawn',
+          href: 'http://www.manasquanschools.org/site/Default.aspx?PageID=816',
+          hrefTxt: 'School Page'
         }
       ],
-      color: "#f0eb81"
+      color: "#b21e1e"
     },
     {
-      title: "How You Can Help",
+      title: "What You'll Learn",
       subHead: [
         {
-          listHead: "Submit Your Email Above, and We'll Send You Information" +
-            " About:",
-          listItem: ["Item Quality Requirements", " Drop-Off or Pick-Up Locations",
-        " How Your Donation will be Used", " Volunteering Information",
-        " Monetary Donation Information", " How to Get in Touch" +
-          " if you have any Questions"]
+          listHead: "Improv teaches more than just how to be funny." +
+            " You'll Learn or Get Better At:",
+          listItem: ["Stage Presence", " Mental Quickness",
+        " Poise Under Pressure", " Quick Decision-Making",
+        " Social Skills", " Verbal/Nonverbal Communication",
+          " Storytelling", " How to Command a Room", " and Much, Much More!"],
+          src: ko.observableArray([]),
+          id: 'pen',
+          href: 'workshops.html',
+          hrefTxt: 'More Info'
         }
       ],
       color: "#15c188"
@@ -54,6 +75,12 @@ var model = {
       
   ]
 };
+
+var getInfo = '<a href="#/">Get More Info Here</a>';
+var picLink = 'http://shorefunimprov.com/photos--shows.html';
+var pics = '</br><a target="_blank" href="' + picLink + '">See Some Pictures!</a>';
+$('#note').append(getInfo);
+$('#outtro-foot').append(pics);
 console.log(model.sections);
 
 var viewModel = {
